99f6b73d3f2f50074f7006eed8bb6ed5c4b407c2
Zisan Ul Haque <149655174+ZisanUlHaque@users.noreply.github.com>
2025-05-19 22:01:39 +0600

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -1,64 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-string getColorWord(const string& status) {
-    return status; 
-}
-
-void bfs(int V, vector<vector<int>>& adj, int start) {
-    vector<string> color(V, "white");
+vector<int> v[1005];
+bool vis[1005];
+void bfs(int src)
+{
     queue<int> q;
-    vector<int> result;
-
-    q.push(start);
-    color[start] = "gray";
-    cout << "Starting BFS from node: " << start << " (status: " << getColorWord("gray") << ")" << endl;
-
-    while (!q.empty()) {
-        int node = q.front();
+    q.push(src);
+    vis[src] = true;
+    while (!q.empty())
+    {
+        int par = q.front();
         q.pop();
-        color[node] = "black";
-        cout << "Visited node: " << node << " (status: " << getColorWord("black") << ")" << endl;
-        result.push_back(node);
-
-        for (int neighbor : adj[node]) {
-            if (color[neighbor] == "white") {
-                color[neighbor] = "gray";
-                q.push(neighbor);
-                cout << "Discovered node: " << neighbor << " from " << node
-                     << " (status: " << getColorWord("gray") << ")" << endl;
+        cout << par << endl;
+        for (int child : v[par])
+        {
+            if (vis[child] == false)
+            {
+                q.push(child);
+                vis[child] = true;
             }
         }
     }
-
-    cout << "Final BFS order: ";
-    for (int x : result)
-        cout << x << " ";
-    cout << endl;
 }
-
-int main() {
-    int V, E;
-    cout << "Enter the number of vertices: ";
-    cin >> V;
+int main()
+{
+    int n, e;
+    cout << "Enter the number of node: ";
+    cin >> n;
     cout << "Enter the number of edges: ";
-    cin >> E;
-
-    vector<vector<int>> adj(V);
-    cout << "Enter edges (u v):" << endl;
-
-    for (int i = 0; i < E; i++) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u); // undirected graph
+    cin >> e;
+    cout << "Enter edges (a b):" << endl;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
     }
-
-    int start;
+    int src;
     cout << "Enter the starting node for BFS: ";
-    cin >> start;
-
-    bfs(V, adj, start);
+    cin >> src;
+    memset(vis, false, sizeof(vis));
+    bfs(src);
     return 0;
 }
 
