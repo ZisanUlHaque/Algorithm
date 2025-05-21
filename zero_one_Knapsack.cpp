8601f7108dc5980d570a23f97e20cf1922c915ca
Zisan Ul Haque <149655174+ZisanUlHaque@users.noreply.github.com>
2025-05-21 21:49:54 +0600

    Update zero_one_Knapsack.cpp

--- zero_one_Knapsack.cpp
+++ zero_one_Knapsack.cpp
@@ -1,24 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Returns the maximum value that can be put in a knapsack of capacity W
-int knapsackRec(int W, vector<int> &val, vector<int> &wt, int n) {
-    if (n == 0 || W == 0)
-        return 0;
-
-    int pick = 0;
-
-    if (wt[n - 1] <= W)
-        pick = val[n - 1] + knapsackRec(W - wt[n - 1], val, wt, n - 1);
-
-    int notPick = knapsackRec(W, val, wt, n - 1);
-
-    return max(pick, notPick);
-}
-
 int knapsack(int W, vector<int> &val, vector<int> &wt) {
     int n = val.size();
-    return knapsackRec(W, val, wt, n);
+    vector<vector<int>> K(n + 1, vector<int>(W + 1, 0));
+
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+
+    return K[n][W];
 }
 
 int main() {
@@ -45,3 +43,4 @@ int main() {
 
     return 0;
 }
+
